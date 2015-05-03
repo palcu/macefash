84fde2335acddfdf40ad02f69bd2bb55a854b365
harababurel <srg.pscs@gmail.com>
2015-05-03 14:46:33 +0300

    added elo rating system, removed voting buttons

--- macefash/routes.py
+++ macefash/routes.py
@@ -16,183 +16,217 @@ def checkAuth(username, password):
     return (username, password) in [('schioara', 'oarba')] #this should probably be moved to the db in the future
 
 def requiresAuth(f):
-  @wraps(f)
-  def decorated(*args, **kwargs):
-    auth = request.authorization
-    if not auth or not checkAuth(auth.username, auth.password):
-      return Response('sorry, can\'t let you in :-(\n', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
-    return f(*args, **kwargs)
-  return decorated
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not checkAuth(auth.username, auth.password):
+            return Response('sorry, can\'t let you in :-(\n', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        return f(*args, **kwargs)
+    return decorated
 """ /AUTH SHIT """
 
 def sh(script):
-  (out, err) = Popen(list(script.split()), stdout=PIPE).communicate()
-  return str(out)
+    (out, err) = Popen(list(script.split()), stdout=PIPE).communicate()
+    return str(out)
 
 def getIP():
-  ip = request.headers.get('X-Real-IP')
-  if ip is None:
-    ip = 'unknown'
-  return ip
+    ip = request.headers.get('X-Real-IP')
+    if ip is None:
+        ip = 'unknown'
+    return ip
 
 def getThemes():
-  return sorted(db.session.query(Theme).all(), key=lambda x: x.name)
+    return sorted(db.session.query(Theme).all(), key=lambda x: x.name)
 
 def getCurrentTheme():
-  themeName = UserSettings.query.filter_by(ip=getIP()).first()
-  themeName = themeName.theme if themeName is not None else "United"
-  themeURL = Theme.query.filter_by(name=themeName).first().source
+    themeName = UserSettings.query.filter_by(ip=getIP()).first()
+    themeName = themeName.theme if themeName is not None else "United"
+    themeURL = Theme.query.filter_by(name=themeName).first().source
 
-  return (themeName, themeURL)
+    return (themeName, themeURL)
 
 def getCurrentGender():
-  currentGender = UserSettings.query.filter_by(ip=getIP()).first()
-  return currentGender.gender if currentGender is not None else False
+    currentGender = UserSettings.query.filter_by(ip=getIP()).first()
+    return currentGender.gender if currentGender is not None else False
 
 def getGenderCount(gender):
-  return len(Person.query.filter_by(gender=gender).all())
+    return len(Person.query.filter_by(gender=gender).all())
+
+def updateRatings(A, B):
+    A = db.session.query(Person).get(A)
+    B = db.session.query(Person).get(B)
+
+    expectedA = 1.0 / (1.0 + 10.0 ** ((B.rating - A.rating)/400.0))
+    expectedB = 1.0 / (1.0 + 10.0 ** ((A.rating - B.rating)/400.0))
+
+    newA = int(A.rating + A.kFactor * (1.0 - expectedA))
+    newB = int(B.rating + B.kFactor * (0.0 - expectedB))
+
+    deltaA = newA - A.rating
+    deltaB = newB - B.rating
+
+    print "new rating for <%s>: %i (delta: %i)\nnew rating for <%s>: %i (delta: %i)" % (A.username, newA, deltaA, B.username, newB, deltaB)
+
+    #other stats
+    A.rating, B.rating = newA, newB
+    A.maxRating, B.maxRating = max(A.maxRating, A.rating), max(B.maxRating, B.rating)
+    A.games, B.games = A.games + 1, B.games + 1
+    A.wins = A.wins + 1
+
+    for X in [A, B]:
+        if X.kFactor == 40 and (X.maxRating >= 2300 or X.games > 30):
+            X.kFactor = 20
+        if X.kFactor == 20 and X.maxRating > 2400:
+            X.kFactor = 10
+
+    db.session.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-  global basePic
+    global basePic
 
-  if request.method == 'POST':
-    print "voted for %i"
+    if request.method == 'POST':
+        A, B = int(request.form['winner']), int(request.form['loser'])
+        print "voted for <%i> instead of <%i>" % (A, B)
 
-  L, R = sample([x for x in db.session.query(Person).filter_by(gender=getCurrentGender()).all()], 2)
+        updateRatings(A, B)
+        return redirect(url_for('home'))
 
-  picL = solveRedirect(basePic % (L.username, 500, 500))
-  picR = solveRedirect(basePic % (R.username, 500, 500))
 
-  return render_template(
-          'home.html',
-          L=L,
-          R=R,
-          picL=picL,
-          picR=picR,
-          ip=getIP(),
-          currentGender=getCurrentGender(),
-          currentTheme=getCurrentTheme(),
-          themes=getThemes(),
-          girls=getGenderCount(False),
-          boys=getGenderCount(True),
-          ungendered=getGenderCount(None),
-          userIP=getIP()
-          )
+    L, R = sample([x for x in db.session.query(Person).filter(Person.gender==getCurrentGender() and Person.hidden==False).all()], 2)
+
+    picL = solveRedirect(basePic % (L.username, 500, 500))
+    picR = solveRedirect(basePic % (R.username, 500, 500))
+
+    return render_template(
+            'home.html',
+            L=L,
+            R=R,
+            picL=picL,
+            picR=picR,
+            ip=getIP(),
+            currentGender=getCurrentGender(),
+            currentTheme=getCurrentTheme(),
+            themes=getThemes(),
+            girls=getGenderCount(False),
+            boys=getGenderCount(True),
+            ungendered=getGenderCount(None),
+            userIP=getIP()
+            )
 
 @app.route('/setTheme/<string:theme>')
 def setTheme(theme):
-  currentUser = UserSettings.query.filter_by(ip=getIP()).first()
-  if currentUser is None:
-    db.session.add(UserSettings(ip=getIP(), theme=theme))
-    print "user %s selected theme <%s> for the first time" % (getIP(), theme)
-  else:
-    currentUser.theme = theme
-    print "user %s changed theme to <%s>" % (getIP(), theme)
+    currentUser = UserSettings.query.filter_by(ip=getIP()).first()
+    if currentUser is None:
+        db.session.add(UserSettings(ip=getIP(), theme=theme))
+        print "user %s selected theme <%s> for the first time" % (getIP(), theme)
+    else:
+        currentUser.theme = theme
+        print "user %s changed theme to <%s>" % (getIP(), theme)
 
-  db.session.commit()
-  return redirect(url_for('home'))
+    db.session.commit()
+    return redirect(url_for('home'))
 
 @app.route('/setGender/<int:gender>')
 def setGender(gender):
-  gender = (gender == 1)
-  currentUser = UserSettings.query.filter_by(ip=getIP()).first()
-  if currentUser is None:
-    db.session.add(UserSettings(ip=getIP(), gender=gender))
-    print "user %s selected gender <%r> for the first time" % (getIP(), gender)
-  else:
-    currentUser.gender = gender
-    print "user %s changed gender to <%r>" % (getIP(), gender)
+    gender = (gender == 1)
+    currentUser = UserSettings.query.filter_by(ip=getIP()).first()
+    if currentUser is None:
+        db.session.add(UserSettings(ip=getIP(), gender=gender))
+        print "user %s selected gender <%r> for the first time" % (getIP(), gender)
+    else:
+        currentUser.gender = gender
+        print "user %s changed gender to <%r>" % (getIP(), gender)
 
-  db.session.commit()
-  return redirect(url_for('home'))
+    db.session.commit()
+    return redirect(url_for('home'))
 
 @app.route('/genderHelp')
 @requiresAuth
 def genderHelp():
-  try:
-    remaining = [x for x in db.session.query(Person).filter(and_(Person.gender==None, Person.hidden==False)).all()]
-    entry = choice(remaining)
-    #entry = db.session.query(Person).filter(and_(Person.gender==None, Person.hidden==False)).first() #by id
-  except:
-    print "no more genders to classify (probably)"
-    return redirect(url_for('home'))
+    try:
+        remaining = [x for x in db.session.query(Person).filter(and_(Person.gender==None, Person.hidden==False)).all()]
+        entry = choice(remaining)
+        #entry = db.session.query(Person).filter(and_(Person.gender==None, Person.hidden==False)).first() #by id
+    except:
+        print "no more genders to classify (probably)"
+        return redirect(url_for('home'))
 
-  pic = solveRedirect(basePic % (entry.username, 400, 400))
-  girls = getGenderCount(False)
-  boys = getGenderCount(True)
-  ungendered = getGenderCount(None)
+    pic = solveRedirect(basePic % (entry.username, 400, 400))
+    girls = getGenderCount(False)
+    boys = getGenderCount(True)
+    ungendered = getGenderCount(None)
 
-  total = len([x for x in db.session.query(Person).all()])
-  classified = total - len(remaining)
+    total = len([x for x in db.session.query(Person).all()])
+    classified = total - len(remaining)
 
-  percentage = float("%.2f" % ((100.0 * classified) / total))
+    percentage = float("%.2f" % ((100.0 * classified) / total))
 
-  return render_template(
-          'genderclassifier.html',
-          x=entry,
-          pic=pic,
-          currentGender=getCurrentGender(),
-          currentTheme=getCurrentTheme(),
-          themes=getThemes(),
-          girls=girls,
-          boys=boys,
-          ungendered=ungendered,
-          percentage=percentage
-          )
+    return render_template(
+            'genderclassifier.html',
+            x=entry,
+            pic=pic,
+            currentGender=getCurrentGender(),
+            currentTheme=getCurrentTheme(),
+            themes=getThemes(),
+            girls=girls,
+            boys=boys,
+            ungendered=ungendered,
+            percentage=percentage
+            )
 
 @app.route('/classifyGender/<string:username>')
 @app.route('/classifyGender/<string:username>/<int:newGender>')
 @requiresAuth
 def classifyGender(username=None, newGender=None):
-  if username is None:
-    print "username is None. dunno wot 2 do :-??"
+    if username is None:
+        print "username is None. dunno wot 2 do :-??"
+        return redirect(url_for('genderHelp'))
+
+    if newGender is not None:
+        if newGender == 3:
+            db.session.query(Person).filter_by(username=username).first().hidden = True
+        else:
+            db.session.query(Person).filter_by(username=username).first().gender = [False, True, None][newGender]
+
+    db.session.commit()
     return redirect(url_for('genderHelp'))
-
-  if newGender is not None:
-    if newGender == 3:
-      db.session.query(Person).filter_by(username=username).first().hidden = True
-    else:
-      db.session.query(Person).filter_by(username=username).first().gender = [False, True, None][newGender]
-
-  db.session.commit()
-  return redirect(url_for('genderHelp'))
 
 @app.route('/all')
 @app.route('/all/<int:page>')
 @requiresAuth
 def showAll(page=None):
-  if page is None: page = 1
-  onPage = 40
+    if page is None: page = 1
+    onPage = 40
 
-  entries = db.session.query(Person).all()
-  #shuffle(entries)
-  pages = len(entries) // onPage + (len(entries) % onPage != 0)
-  firstNav, lastNav = max(1, page-3), min(page+3, pages)
-  shownEntries = entries[(page-1)*onPage: min(len(entries), page*onPage)]
+    entries = db.session.query(Person).all()
+    #shuffle(entries)
+    pages = len(entries) // onPage + (len(entries) % onPage != 0)
+    firstNav, lastNav = max(1, page-3), min(page+3, pages)
+    shownEntries = entries[(page-1)*onPage: min(len(entries), page*onPage)]
 
-  return render_template(
-          'all.html',
-          entries=shownEntries,
-          page=page,
-          pages=pages,
-          firstNav=firstNav,
-          lastNav=lastNav,
-          currentTheme=getCurrentTheme(),
-          themes=getThemes(),
-          girls=getGenderCount(False),
-          boys=getGenderCount(True),
-          ungendered=getGenderCount(None)
-          )
+    return render_template(
+            'all.html',
+            entries=shownEntries,
+            page=page,
+            pages=pages,
+            firstNav=firstNav,
+            lastNav=lastNav,
+            currentTheme=getCurrentTheme(),
+            themes=getThemes(),
+            girls=getGenderCount(False),
+            boys=getGenderCount(True),
+            ungendered=getGenderCount(None)
+            )
 
 @app.errorhandler(404)
 def pageNotFound(e):
-  return render_template(
-          '404.html',
-          currentTheme=getCurrentTheme(),
-          themes=getThemes(),
-          girls=getGenderCount(False),
-          boys=getGenderCount(True),
-          ungendered=getGenderCount(None)
-          ), 404
+    return render_template(
+            '404.html',
+            currentTheme=getCurrentTheme(),
+            themes=getThemes(),
+            girls=getGenderCount(False),
+            boys=getGenderCount(True),
+            ungendered=getGenderCount(None)
+            ), 404
 
