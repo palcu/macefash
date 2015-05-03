fe3a56aedb93564f5bc76cf30f21d8f0d77732c4
harababurel <srg.pscs@gmail.com>
2015-05-03 15:38:16 +0300

    all indentation is 4 spaces

--- macefash/models.py
+++ macefash/models.py
@@ -1,101 +1,101 @@
 from __init__ import app, db
 
 class Person(db.Model):
-  __tablename__ = "persons"
+    __tablename__ = "persons"
 
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String, unique=True, nullable=False)
-  gender = db.Column(db.Boolean, unique=False, nullable=True)
-  city = db.Column(db.String, unique=False, nullable=True)
-  school = db.Column(db.String, unique=False, nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    gender = db.Column(db.Boolean, unique=False, nullable=True)
+    city = db.Column(db.String, unique=False, nullable=True)
+    school = db.Column(db.String, unique=False, nullable=True)
 
-  rating = db.Column(db.Integer, unique=False, nullable=True)
-  maxRating = db.Column(db.Integer, unique=False, nullable=True)
-  kFactor = db.Column(db.Integer, unique=False, nullable=True)
+    rating = db.Column(db.Integer, unique=False, nullable=True)
+    maxRating = db.Column(db.Integer, unique=False, nullable=True)
+    kFactor = db.Column(db.Integer, unique=False, nullable=True)
 
-  games = db.Column(db.Integer, unique=False, nullable=True)
-  wins = db.Column(db.Integer, unique=False, nullable=True)
+    games = db.Column(db.Integer, unique=False, nullable=True)
+    wins = db.Column(db.Integer, unique=False, nullable=True)
 
-  hidden = db.Column(db.Boolean, unique=False, nullable=True)
+    hidden = db.Column(db.Boolean, unique=False, nullable=True)
 
-  def __init__(self, username, gender=None, city=None, school=None, rating=0, maxRating=0, kFactor=40, games=0, wins=0, hidden=False):
-    self.username = username
-    self.gender = gender
-    self.city = city
-    self.school = school
-    self.rating = rating
-    self.maxRating = maxRating
-    self.kFactor = kFactor
-    self.games = games
-    self.wins = wins
-    self.hidden = hidden
+    def __init__(self, username, gender=None, city=None, school=None, rating=0, maxRating=0, kFactor=40, games=0, wins=0, hidden=False):
+        self.username = username
+        self.gender = gender
+        self.city = city
+        self.school = school
+        self.rating = rating
+        self.maxRating = maxRating
+        self.kFactor = kFactor
+        self.games = games
+        self.wins = wins
+        self.hidden = hidden
 
-  def __repr__(self):
-    return self.username
+    def __repr__(self):
+        return self.username
 
 class Vote(db.Model):
-  __tablename__ = 'votes'
+    __tablename__ = 'votes'
 
-  id = db.Column(db.Integer, primary_key=True)
-  ip = db.Column(db.String, unique=False, nullable=True)
-  firstOption = db.Column(db.Integer, unique=False, nullable=True)
-  secondOption = db.Column(db.Integer, unique=False, nullable=True)
-  winner = db.Column(db.Integer, unique=False, nullable=True)
-  timestamp = db.Column(db.String, unique=False, nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String, unique=False, nullable=True)
+    firstOption = db.Column(db.Integer, unique=False, nullable=True)
+    secondOption = db.Column(db.Integer, unique=False, nullable=True)
+    winner = db.Column(db.Integer, unique=False, nullable=True)
+    timestamp = db.Column(db.String, unique=False, nullable=True)
 
-  def __init__(self, ip, firstOption, secondOption, winner, timestamp):
-    self.ip = ip
-    self.firstOption = firstOption
-    self.secondOption = secondOption
-    self.winner = winner
-    self.timestamp = timestamp
+    def __init__(self, ip, firstOption, secondOption, winner, timestamp):
+        self.ip = ip
+        self.firstOption = firstOption
+        self.secondOption = secondOption
+        self.winner = winner
+        self.timestamp = timestamp
 
-  def __repr__(self):
-    return "%s rolled (%i, %i) and voted for %i" (self.ip, self.firstOption, self.secondOption, self.winner)
+    def __repr__(self):
+        return "%s rolled (%i, %i) and voted for %i" (self.ip, self.firstOption, self.secondOption, self.winner)
 
 class Message(db.Model):
-  __tablename__ = 'messages'
+    __tablename__ = 'messages'
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String, unique=False, nullable=True)
-  ip = db.Column(db.String, unique=False, nullable=True)
-  message = db.Column(db.String, unique=False, nullable=False)
-  timestamp = db.Column(db.String, unique=False, nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=False, nullable=True)
+    ip = db.Column(db.String, unique=False, nullable=True)
+    message = db.Column(db.String, unique=False, nullable=False)
+    timestamp = db.Column(db.String, unique=False, nullable=True)
 
-  def __init__(self, name, ip, message, timestamp):
-    self.name = name
-    self.ip = ip
-    self.message = message
-    self.timestamp = timestamp
+    def __init__(self, name, ip, message, timestamp):
+        self.name = name
+        self.ip = ip
+        self.message = message
+        self.timestamp = timestamp
 
-  def __repr__(self):
-    return "%s (ip = %s) wrote: %s" % (self.name, self.ip, self.message)
+    def __repr__(self):
+        return "%s (ip = %s) wrote: %s" % (self.name, self.ip, self.message)
 
 class Theme(db.Model):
-  __tablename__ = 'themes'
+    __tablename__ = 'themes'
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String, unique=True, nullable=False)
-  source = db.Column(db.String, unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    source = db.Column(db.String, unique=True, nullable=False)
 
-  def __init__(self, name, source):
-      self.name = name
-      self.source = source
+    def __init__(self, name, source):
+        self.name = name
+        self.source = source
 
-  def __repr__(self):
-      return "themeName: <%s>" % self.name
+    def __repr__(self):
+        return "themeName: <%s>" % self.name
 
 class UserSettings(db.Model):
-  __tablename__ = 'usersettings'
+    __tablename__ = 'usersettings'
 
-  ip = db.Column(db.String, primary_key=True, nullable=True)
-  theme = db.Column(db.String, unique=False, nullable=False)
-  gender = db.Column(db.Boolean, unique=False, nullable=True)
+    ip = db.Column(db.String, primary_key=True, nullable=True)
+    theme = db.Column(db.String, unique=False, nullable=False)
+    gender = db.Column(db.Boolean, unique=False, nullable=True)
 
-  def __init__(self, ip, theme=None, gender=None):
-    self.ip = ip
-    self.theme = theme if theme is not None else "Standard"
-    self.gender = gender if gender is not None else False #aka gurls
+    def __init__(self, ip, theme=None, gender=None):
+        self.ip = ip
+        self.theme = theme if theme is not None else "Standard"
+        self.gender = gender if gender is not None else False #aka gurls
 
-  def __repr__(self):
-      return "ip %s wants:\n---> theme: <%s>\n---> gender: %r" % (self.ip, self.theme, self.gender)
+    def __repr__(self):
+        return "ip %s wants:\n---> theme: <%s>\n---> gender: %r" % (self.ip, self.theme, self.gender)
--- macefash/redirectSolver.py
+++ macefash/redirectSolver.py
@@ -2,25 +2,25 @@ import urlparse
 import httplib
 
 def solveRedirect(url, depth=0):
-  """
-  Facebook API provides a URL for acquiring profile pictures.
-  That URL contains a person's username, which is not wanted.
-  However, the URL redirects to an encrypted link, which can then be used.
-  This function gets the encrypted address
-  """
+    """
+    Facebook API provides a URL for acquiring profile pictures.
+    That URL contains a person's username, which is not wanted.
+    However, the URL redirects to an encrypted link, which can then be used.
+    This function gets the encrypted address
+    """
 
-  if depth > 2:
-    raise Exception("redirected %i times, giving up." % depth)
-  o = urlparse.urlparse(url, allow_fragments=True)
-  conn = httplib.HTTPConnection(o.netloc)
-  path = o.path
-  if o.query:
-    path += '?' + o.query
+    if depth > 2:
+        raise Exception("redirected %i times, giving up." % depth)
+    o = urlparse.urlparse(url, allow_fragments=True)
+    conn = httplib.HTTPConnection(o.netloc)
+    path = o.path
+    if o.query:
+        path += '?' + o.query
 
-  conn.request("HEAD", path)
-  res = conn.getresponse()
-  headers = dict(res.getheaders())
+    conn.request("HEAD", path)
+    res = conn.getresponse()
+    headers = dict(res.getheaders())
 
-  if headers.has_key('location') and headers['location'] != url:
-    return solveRedirect(headers['location'], depth+1)
-  return url
+    if headers.has_key('location') and headers['location'] != url:
+        return solveRedirect(headers['location'], depth+1)
+    return url
